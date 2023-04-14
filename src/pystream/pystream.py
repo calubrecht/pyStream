a085476b2509e1cb526bf4fef4883184a7f8c51f
Chad Lubrecht <chad@calubrecht.us>
2023-04-14 13:05:16 -0400

    Add type hints

--- src/pystream/pystream.py
+++ src/pystream/pystream.py
@@ -1,65 +1,81 @@
 from functools import reduce
 from itertools import groupby
-from itertools import chain
+from collections.abc import Iterable
+from typing import Callable
 
-def terminalMethod(func):
+
+def terminal_method(func):
     def wrapper(*args):
-        streamObj = args[0]
+        stream_obj = args[0]
         res = func(*args)
-        streamObj.close()
+        stream_obj.close()
         return res
+
     return wrapper
 
+
 class Stream:
-    def __init__(self, streamable):
+    def __init__(self, streamable: Iterable):
         self.streamable = streamable
         self.iter = None
 
     def __iter__(self):
         if not self.iter:
-          self.iter = iter(self.streamable)
+            self.iter = iter(self.streamable)
         return self.iter
 
     def __next__(self):
         if not self.iter:
-          self.iter = iter(self.streamable)
+            self.iter = iter(self.streamable)
         return next(self.iter)
 
-    def map(self, mappedFunc):
-        newStreamable = map(mappedFunc, self.streamable)
-        return Stream(newStreamable)
+    def map(self, mapped_func: Callable) -> 'Stream':
+        new_streamable = map(mapped_func, self.streamable)
+        return Stream(new_streamable)
 
-    def filter(self, filterFunc):
-        newStreamable = filter(filterFunc, self.streamable)
-        return Stream(newStreamable)
+    def filter(self, filter_func: Callable) -> 'Stream':
+        new_streamable = filter(filter_func, self.streamable)
+        return Stream(new_streamable)
 
-    def zip(self, *otherStreams):
-        streamables = [self.streamable,] + list(Stream(otherStreams).map(lambda s : s.streamable))
-        newStreamable = zip(*streamables)
-        return Stream(newStreamable)
+    def zip(self, *other_streams: Iterable) -> 'Stream':
+        for stream in other_streams:
+            if not isinstance(stream, Iterable):
+                raise TypeError("Input " + str(stream) + " is not an Iterable")
+        streamables = [self.streamable, ] + list(Stream(other_streams).map(lambda s: to_stream(s)))
+        new_streamable = zip(*streamables)
+        return Stream(new_streamable)
 
-    @terminalMethod
-    def group(self, keyFunc):
+    @terminal_method
+    def group(self, key_func: Callable) -> dict:
         grouped = {}
-        for k,g in groupby(self.streamable, keyFunc):
+        for k, g in groupby(self.streamable, key_func):
             grouped[k] = list(g)
         return grouped
 
-    @terminalMethod
+    @terminal_method
     def first(self):
-        return next(self.streamable)
+        return next(iter(self.streamable))
 
-    @terminalMethod
-    def reduce(self, reduceFunc):
-        return reduce(reduceFunc, self.streamable)
+    @terminal_method
+    def reduce(self, reduce_func: Callable):
+        return reduce(reduce_func, self.streamable)
 
     def close(self):
         self.streamable = []
 
 
-def zipStreams(*streams):
+def to_stream(streamable: Iterable) -> Stream:
+    if isinstance(streamable, Stream):
+        return streamable
+    return Stream(streamable)
+
+
+def zip_streams(*streams: Iterable) -> Stream:
     if len(streams) == 0:
         return Stream([])
+    for s in streams:
+        if not isinstance(s, Iterable):
+            raise TypeError("Input <" + str(s) + "> is not an iterable")
     if len(streams) == 1:
-        return streams[0]
-    return streams[0].zip(*streams[1:])
+        return to_stream(streams[0])
+    return to_stream(streams[0]).zip(*streams[1:])
--- tests/test_pystream.py
+++ tests/test_pystream.py
@@ -1,109 +1,120 @@
 import unittest
 from src.pystream import Stream
-from src.pystream import zipStreams
+from src.pystream import zip_streams
+
 
 class SimpleData:
-  def __init__(self, objName, classification, count):
-    self.objName = objName
-    self.classification = classification
-    self.count = count
+    def __init__(self, objName, classification, count):
+        self.objName = objName
+        self.classification = classification
+        self.count = count
+
 
 class TestPystream(unittest.TestCase):
 
-  def test_chainFilterMap(self):
-    data = (SimpleData("Bread", "Food", 23), SimpleData("Corn", "Food", 11), SimpleData("Robot", "Toy", 3))
+    def test_chainFilterMap(self):
+        data = (SimpleData("Bread", "Food", 23), SimpleData("Corn", "Food", 11), SimpleData("Robot", "Toy", 3))
 
-    mapped = Stream(data).filter(lambda d : d.classification=="Food").map(lambda d : str(d.count) + " " + d.objName + "s")
+        mapped = Stream(data).filter(lambda d: d.classification == "Food").map(
+            lambda d: str(d.count) + " " + d.objName + "s")
 
-    mappedList = list(mapped)
+        mappedList = list(mapped)
 
-    self.assertEqual(2, len(mappedList))
-    self.assertEqual("23 Breads", mappedList[0])
+        self.assertEqual(2, len(mappedList))
+        self.assertEqual("23 Breads", mappedList[0])
 
-  def test_streamFirst(self):
-    data = (SimpleData("Bread", "Food", 23), SimpleData("Corn", "Food", 11), SimpleData("Robot", "Toy", 3))
+    def test_streamFirst(self):
+        data = (SimpleData("Bread", "Food", 23), SimpleData("Corn", "Food", 11), SimpleData("Robot", "Toy", 3))
 
-    stream = Stream(data).filter(lambda d : d.classification=="Food")
-    firstItem= stream.first()
-    self.assertEqual("Bread", firstItem.objName)
+        stream = Stream(data).filter(lambda d: d.classification == "Food")
+        firstItem = stream.first()
+        self.assertEqual("Bread", firstItem.objName)
 
-    # stream is now finished, should have nothing to return
-    with self.assertRaises(StopIteration):
-      next(stream)
+        # stream is now finished, should have nothing to return
+        with self.assertRaises(StopIteration):
+            next(stream)
 
-  def test_groupStream(self):
-    data = (SimpleData("Bread", "Food", 23), SimpleData("Corn", "Food", 11), SimpleData("Robot", "Toy", 3))
-    groups = Stream(data).group(lambda d: d.classification)
+    def test_groupStream(self):
+        data = (SimpleData("Bread", "Food", 23), SimpleData("Corn", "Food", 11), SimpleData("Robot", "Toy", 3))
+        groups = Stream(data).group(lambda d: d.classification)
 
-    self.assertEqual(2, len(groups))
-    self.assertEqual(2, len(groups["Food"]))
-    self.assertEqual(1, len(groups["Toy"]))
+        self.assertEqual(2, len(groups))
+        self.assertEqual(2, len(groups["Food"]))
+        self.assertEqual(1, len(groups["Toy"]))
 
-  def test_zip(self):
-    data1 = (SimpleData("Bread", "Food", 23), SimpleData("Corn", "Food", 11), SimpleData("Robot", "Toy", 3))
-    data2 = (1, 2)
+    def test_zip(self):
+        data1 = (SimpleData("Bread", "Food", 23), SimpleData("Corn", "Food", 11), SimpleData("Robot", "Toy", 3))
+        data2 = (1, 2)
 
-    zipped = list(zipStreams(Stream(data1).filter(lambda d : d.classification == "Food"), Stream(data2)))
+        zipped = list(zip_streams(Stream(data1).filter(lambda d: d.classification == "Food"), Stream(data2)))
 
-    self.assertEqual(2, len(zipped))
-    self.assertEqual("Bread", zipped[0][0].objName)
-    self.assertEqual(1, zipped[0][1])
-    self.assertEqual("Corn", zipped[1][0].objName)
-    self.assertEqual(2, zipped[1][1])
+        self.assertEqual(2, len(zipped))
+        self.assertEqual("Bread", zipped[0][0].objName)
+        self.assertEqual(1, zipped[0][1])
+        self.assertEqual("Corn", zipped[1][0].objName)
+        self.assertEqual(2, zipped[1][1])
 
-  def test_zipFromStreamable(self):
-    stream1 = Stream((23, 100, 97))
-    stream2 = Stream(("A", "B", "C"))
+    def test_zipFromStreamable(self):
+        stream1 = Stream((23, 100, 97))
+        stream2 = Stream(("A", "B", "C"))
 
-    zipped = list(stream1.zip(stream2))
+        zipped = list(stream1.zip(stream2))
 
-    self.assertEqual(3, len(zipped))
-    self.assertEqual((23, "A"), zipped[0])
-    self.assertEqual((97, "C"), zipped[2])
+        self.assertEqual(3, len(zipped))
+        self.assertEqual((23, "A"), zipped[0])
+        self.assertEqual((97, "C"), zipped[2])
 
-  def test_zipMoreStreams(self):
-    stream1 = Stream((23, 100, 97))
-    stream2 = Stream(("A", "B", "C"))
-    stream3 = Stream((3, 2, 1))
+    def test_zipMoreStreams(self):
+        stream1 = Stream((23, 100, 97))
+        stream2 = Stream(("A", "B", "C"))
+        stream3 = Stream((3, 2, 1))
 
-    zipped = list(zipStreams(stream1, stream2, stream3))
+        zipped = list(zip_streams(stream1, stream2, stream3))
 
-    self.assertEqual(3, len(zipped))
-    self.assertEqual((23, "A", 3), zipped[0])
-    self.assertEqual((97, "C", 1), zipped[2])
+        self.assertEqual(3, len(zipped))
+        self.assertEqual((23, "A", 3), zipped[0])
+        self.assertEqual((97, "C", 1), zipped[2])
 
-  def test_lazyExecution(self):
-    was_run_a = False
-    was_run_b = False
+    def test_lazyExecution(self):
+        was_run_a = False
+        was_run_b = False
 
-    def filterA(i):
-      nonlocal was_run_a
-      was_run_a = True
-      return True
-    def filterB(i):
-      nonlocal was_run_b
-      was_run_b = True
-      return True
+        def filterA(i):
+            nonlocal was_run_a
+            was_run_a = True
+            return True
 
-    stream1 = Stream(('A', 'B', 'C'))
-    stream2 = Stream((1, 2, 3))
+        def filterB(i):
+            nonlocal was_run_b
+            was_run_b = True
+            return True
 
-    out1 = stream1.filter(filterA)
+        stream1 = Stream(('A', 'B', 'C'))
+        stream2 = Stream((1, 2, 3))
 
-    self.assertFalse(was_run_a)
+        out1 = stream1.filter(filterA)
 
-    list(out1)
+        self.assertFalse(was_run_a)
 
-    self.assertTrue(was_run_a)
+        list(out1)
 
-    was_run_a = False
+        self.assertTrue(was_run_a)
 
-    zippedOut = zipStreams(stream1.filter(filterA), stream2.filter(filterB))
+        was_run_a = False
 
-    self.assertFalse(was_run_a)
-    self.assertFalse(was_run_b)
+        zippedOut = zip_streams(stream1.filter(filterA), stream2.filter(filterB))
 
-    list(zippedOut)
+        self.assertFalse(was_run_a)
+        self.assertFalse(was_run_b)
 
-    self.assertTrue(was_run_a)
-    self.assertTrue(was_run_b)
+        list(zippedOut)
+
+        self.assertTrue(was_run_a)
+        self.assertTrue(was_run_b)
+
+    def test_typeErrors(self):
+        s1 = 21
+        s2 = 33
+        with self.assertRaises(TypeError) as te:
+            zip_streams(s1, s2)
+        self.assertEqual("Input <21> is not an iterable", te.exception.args[0])
